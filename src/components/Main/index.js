bead2cb4853b1015f386e2c2a4ab1da12f7817fb
keirastanley <keiraestanley@gmail.com>
2022-11-10 16:55:21 +0000

    tasks

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -8,8 +8,10 @@ import Post from '../Post';
 
 function Main() {
   const [posts, setPosts] = useState(samplePosts);
-
-  return <main id="main"></main>;
+  return <main id="main">
+  {posts.map(post => 
+  <Post post={post} key={post.postId}/>)}
+  </main>;    
 }
 
 export default Main;
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 import './index.css';
 
-function Post() {
-  return <article></article>;
+function Post(props) {
+  const post = props.post
+  return <article>
+    <h1>{post.title}</h1>
+    <h2>{post.author}</h2>
+    <p>{post.date}</p>
+    <img src={post.image.link} alt={post.image.alt}/>
+    <p>{post.text}</p>
+  </article>;
 }
 
 export default Post;
